1e06024a0e599d203da81dccc25d8dfd93e9b002
sthanleyso <sthanleys7@gmail.com>
2018-12-14 14:22:45 -0300

    Adicionado tela de favoritos e o player de músicas

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,13 +9,13 @@ class DashboardController < ApplicationController
 
 
   def load_recent_heard
-    @recent_albums = current_user.recently_heards.order("created_at DESC").limit(4).map(&:album)
+    @recent_albums = current_user.recently_heards.order("created_at DESC").group(:id).limit(4).map(&:album)
   end
 
 
   def load_recommendations
     heard_categories = @recent_albums.map(&:category)
     @recommended_albums = Album.joins(:category, :songs).where(category: heard_categories).order("songs.played_count")
-                               .limit(4)
+                               .select("albums.*").limit(4)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get 'recently_heards/create'
   devise_for :users
   
   authenticated :user do
+    resources :favorites, only: :index
     root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
     resources :artists, only: :show
-    resources :albums, only: :show
+    resources :albums, only: :show do
+      resources :recently_heards, only: :create
+    end
+    resources :songs, only: [] do
+      post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+      delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+    end
   end
 
   unauthenticated :user do
